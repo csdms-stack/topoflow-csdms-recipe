796dc05adf12b14baaf78ac183a03a80d9f45fa8
mcflugen <mcflugen@gmail.com>
2016-10-12 10:14:46 -0600

    Updated component test.

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -4,16 +4,10 @@ import os
 os.mkdir('_testing')
 os.chdir('_testing')
 
-import pymt.components
+from pymt.components import ChannelsDiffusiveWave as Model
 
-components = pymt.components.__all__
-print('found {num} components'.format(num=len(components)))
+model = Model()
 
-for name in components:
-    print('instantiate: {name}'.format(name=name))
-    cls = pymt.components.__dict__[name]
-    model = cls()
-
-    for default in model.defaults:
-        print('{name}: {val} {units}'.format(
-            name=default[0], val=default[1][0], units=default[1][1]))
+for default in model.defaults:
+    print('{name}: {val} {units}'.format(
+        name=default[0], val=default[1][0], units=default[1][1]))
